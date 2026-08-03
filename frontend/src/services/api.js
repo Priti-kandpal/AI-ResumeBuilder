e6659473671ae595e61e0442093822c5ca556eab
Priti-Kandpal <kandpalpreeti08@gmail.com>
2026-08-03 17:14:23 +0530

    updated backend api after deploying on render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { message } from 'antd';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://ai-resumebuilder-1-qff5.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
